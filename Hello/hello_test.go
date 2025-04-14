cf35f8a53039c411263bf5824cdb0dc836b57e2d
ermilova <ermilova@rutoken.ru>
2025-04-14 19:09:34 +0300

    ADD test

--- Hello/hello_test.go
+++ Hello/hello_test.go
@@ -1,5 +1,16 @@
-package main
+package Hello
 
-func main() {
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
+func TestEnglish(t *testing.T) {
+	name := "Ben"
+	language := "english"
+	expected := "Hello Ben"
+	actual, err := Hello(name, language)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expected, actual)
 }
